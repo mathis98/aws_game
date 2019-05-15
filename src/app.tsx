145948ccd8bc82f0450d0c169338a013d015cecc
Jan Beckschewe <jan.becksy@gmail.com>
2019-05-15 15:04:28 +0200

    Try to fix routing problem

--- src/app.tsx
+++ src/app.tsx
@@ -7,17 +7,18 @@ import { StartPage } from "./pages/StartPage";
 import { GamePage } from "./pages/GamePage";
 import { LevelsPage } from "./pages/LevelsPage";
 import { InstructionsPage } from "./pages/InstructionsPage";
+import {BrowserRouter} from "react-router-dom";
 
 // the history required for the Router element
 const history = createBrowserHistory();
 
 // the routes
 ReactDOM.render(
-  <Router history={history}>
+  <BrowserRouter basename="/calendar">
     <Route path="/" exact component={StartPage} />
     <Route path="/game" exact component={GamePage} />
     <Route path="/levels" exact component={LevelsPage} />
     <Route path="/instruction" exact component={InstructionsPage} />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById("app")
 );
