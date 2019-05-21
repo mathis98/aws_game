1b40c27286696581db3298de96682873e5222885
Mathis <mathis.arend@gmail.com>
2019-05-21 15:24:16 +0200

    Finish drag and drop demo

--- src/components/Droppable.tsx
+++ src/components/Droppable.tsx
@@ -13,10 +13,11 @@ const squareTarget = {
       return true;
   },
   drop(props: any, monitor: any, component: any) {
-    console.log(`Drop area ${props.data.id} is being dropped on by Draggable ${monitor.getItem().id}`);
-    console.log(monitor.getItem());
     props.data.bg = monitor.getItem().color;
     props.data.text = monitor.getItem().text;
+    if(monitor.getItem().id == props.data.accepts)
+      props.setDroppableDone(props.data.id, true);
+    else props.setDroppableDone(props.data.id, false);
   }
 };
 
@@ -33,6 +34,7 @@ export interface DroppableProps {
   isOver?: boolean;
   canDrop?: boolean;
   data: any;
+  setDroppableDone: any;
 }
 
 class Droppable extends React.Component<DroppableProps, {}> {
--- src/pages/dndShowcase.tsx
+++ src/pages/dndShowcase.tsx
@@ -28,25 +28,37 @@ var draggables: any = [
 var droppables: any = [
   {
     'id': 1,
-    'accepts': 'a'
+    'accepts': 'a',
+    'done': false
   },
   {
     'id': 2,
-    'accepts': 'b'
+    'accepts': 'b',
+    'done': false
   }
 ]
 
 // 'StartPageProps' describes the shape of props.
 // State is never set so we use the '{}' type.
 export class dndShowcase extends React.Component<dndShowcaseProps, {}> {
+
+  droppableDone = (id: string, correct: boolean) => {
+    if(correct) {
+      droppables.find((a: any) => a.id == id).done = true;
+      if(droppables.every((a: any) => a.done))
+        alert('Du hast gewonnen!');
+    }
+    else droppables.find((a: any) => a.id == id).done = false;
+  }
+
   render() {
     return (
       <div className={css.component_big}>
         <Typography variant="h4" gutterBottom>
           Beispielseite für Drag 'n' Drop
         </Typography>
-        <Droppable data={droppables[0]}/>
-        <Droppable data={droppables[1]}/>
+        <Droppable data={droppables[0]} setDroppableDone={this.droppableDone}/>
+        <Droppable data={droppables[1]} setDroppableDone={this.droppableDone}/>
         <Draggable data={draggables[0]}/>
         <Draggable data={draggables[1]}/>
         <p>
