60fb3ce329a834da599af8fec86666c06a8bf4ee
Mathis <mathis.arend@gmail.com>
2019-05-22 14:37:04 +0200

    Hide Drag n Drop example Page from StartPage

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -29,11 +29,6 @@ export class StartPage extends React.Component<StartPageProps, {}> {
               Level wählen
             </LinkButton>
           </div>
-          <div className={css.start_button_wrapper}>
-            <LinkButton className={css.start_button} variant="contained" size="large" color="primary" to="/dnd">
-              Drag 'n' Drop
-            </LinkButton>
-          </div>
         </div>
       </div>
     );
