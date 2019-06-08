edee6067f3fb84447b77d8cd35a1e9431806f8dd
Mathis Arend <mathis.arend@gmail.com>
2019-06-08 20:15:07 +0000

    Implementierung von Level 3

--- src/levels/levels.tsx
+++ src/levels/levels.tsx
@@ -1,4 +1,5 @@
 import { level1 } from 'levels/level1';
 import { level2 } from 'levels/level2';
+import { level3 } from 'levels/level3';
 
-export default [level1, level2];
+export default [level1, level2, level3];
