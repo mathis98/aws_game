1447c18a2e8c677f3e95e2aa104ad6c818739b77
Jan Be <jan.becksy@gmail.com>
2019-06-09 10:28:44 +0000

    Set correct info popup for each level

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,21 +1,26 @@
 import * as React from 'react';
-import { withRouter } from 'react-router-dom';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import MarkdownViewer from 'components/MarkdownViewer';
 
-const { default: instructions } = require("level_data/level_1/popup.md");
+let instructions : string;
 
-class Popup extends React.Component<any, any> {
+export interface PopupProps extends RouteComponentProps<any> {
+  levelId?: number;
+}
+
+class Popup extends React.Component<PopupProps, any> {
   handleNextBound: any;
   handleCancelBound: any;
 
   constructor(props: any) {
     super(props);
     this.state = {open: true};
+
+    instructions = require(`level_data/level_${this.props.levelId}/popup.md`).default;
 
     // This binding is necessary to make `this` work in the callback
     this.handleNextBound = this.handleNext.bind(this);
