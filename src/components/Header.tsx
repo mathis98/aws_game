8beafe10ff1a5a13816fcdf88a4b80011111d2e0
Leon Wiemers <leonwiemers@gmail.com>
2019-06-10 12:34:43 +0200

    fix header css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,18 +8,18 @@ const css = require('./Header.css');
 
 export interface HeaderProps {}
 
-// 'HeaderProps' describes the shape of props.
-// State is never set so we use the '{}' type.
 export default class Header extends React.Component<HeaderProps, any> {
   render() {
     return (
       <AppBar position="static" className={css.fixed_size_app_bar}>
         <Toolbar className={css.toolBar}>
-          <Typography variant="h6" color="inherit">
-            AWS Boot Camp
-          </Typography>
+          <Link to="/" className={css.link}>
+            <Typography variant="h6" color="inherit" className={css.toolBarName}>
+              AWS Boot Camp
+            </Typography>
+          </Link>
 
-          <Typography variant="h7" color="inherit">
+          <Typography variant="h6" color="inherit">
             42 Punkte
           </Typography>
         </Toolbar>
