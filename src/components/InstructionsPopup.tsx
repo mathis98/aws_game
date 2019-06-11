6e4c8cf2785bf658826ddf8c0fd677f0d740b411
Jan Beckschewe <jan.becksy@gmail.com>
2019-06-12 00:51:50 +0200

    Merge master into success-popup

---
+++ src/components/InstructionsPopup.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import MarkdownViewer from 'components/MarkdownViewer';
+
+export interface InstructionsPopupProps extends RouteComponentProps<any> {
+  instructionsMd: string;
+}
+
+interface InstructionsPopupState {
+  open?: boolean;
+}
+
+class InstructionsPopup extends React.Component<InstructionsPopupProps, InstructionsPopupState> {
+  constructor(props: InstructionsPopupProps) {
+    super(props);
+    this.state = {open: true};
+
+    this.handleNext = this.handleNext.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+
+  render() {
+    return (
+      <Dialog open={this.state.open} maxWidth="md">
+        <DialogContent>
+          <MarkdownViewer source={this.props.instructionsMd} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleCancel} color="secondary">
+            Abbruch
+          </Button>
+          <Button onClick={this.handleNext} color="primary" autoFocus>
+            Weiter
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+
+  handleNext() {
+    this.setState({open: false});
+  }
+
+  handleCancel() {
+    this.props.history.push('/');
+  }
+}
+
+export default withRouter(InstructionsPopup);
---
+++ src/levels/level3.tsx
@@ -0,0 +1,85 @@
+import { Level, LevelFeedback, LevelState } from './level'
+import * as React from 'react'
+
+const level3: Level ={
+  columns: 6,
+  rows: [2, 2, 1, 2, 2],
+  gap: "2em",
+  elements: [
+    {
+      position: {
+        column: 1,
+        row: 1
+      },
+      id: "users",
+      icon: "users"
+    },
+    {
+      position: {
+        column: 2,
+        row: 1
+      },
+      id: "shield",
+      droppable: true
+    },
+    {
+      position: {
+        column: 3,
+        row: 1
+      },
+      id: "iam",
+      droppable: true
+    },
+    {
+      position: {
+        column: 4,
+        row: 1
+      },
+      id: "gameserver",
+      icon: "gameserver"
+    },
+    {
+      position: {
+        column: 3,
+        row: 3
+      },
+      id: "dynamo",
+      droppable: true
+    }
+  ],
+  relations: [
+    {
+      sourceId: "users",
+      targetId: "shield",
+      sourceAnchor: "right",
+      targetAnchor: "left"
+    },
+    {
+      sourceId: "shield",
+      targetId: "iam",
+      sourceAnchor: "right",
+      targetAnchor: "left"
+    },
+    {
+      sourceId: "iam",
+      targetId: "gameserver",
+      sourceAnchor: "right",
+      targetAnchor: "left"
+    },
+    {
+      sourceId: "iam",
+      targetId: "dynamo",
+      sourceAnchor: "bottom",
+      targetAnchor: "top",
+      doubleArrow: true
+    }
+  ],
+  awspalette: ["iam", "shield", "dynamodb", "s3"],
+  validator: Level3Validator
+}
+
+export default level3;
+
+function Level3Validator(state: LevelState): LevelFeedback {
+  return {correct: false, feedbackComponent: <span>not yet implemented</span>};
+}
