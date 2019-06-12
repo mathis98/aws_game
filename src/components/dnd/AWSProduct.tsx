539f39a689e30f95f87ff2cab57f958e8d0eb085
Dominic <d.schialer@campus.tu-berlin.de>
2019-06-12 19:49:33 +0000

    added separate background color for AWS icons

--- src/components/dnd/AWSProduct.tsx
+++ src/components/dnd/AWSProduct.tsx
@@ -5,12 +5,17 @@ const css = require('./AWSProduct.css');
 export interface AWSProductProps {
   icon: string;
   text: string;
+  color: string;
+}
+const colors: any = {
+  "s3": "#3F8624",
+  "dynamodb": "#3B48CC"
 }
 
 export default class AWSProduct extends React.Component<AWSProductProps, {}> {
   render() {
     return (
-      <div className={css.awsProduct}>
+      <div className={css.awsProduct} style={{ backgroundColor: this.props.color}}>
         <img src={require(`assets/img/${this.props.icon}.svg`) as string} className={css.awsProduct} />
         <span className={css.awsProductText}>{this.props.text}</span>
       </div>
