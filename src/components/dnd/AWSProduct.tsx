78fcb7bad892266b9663bda545bb2952e31b33dc
Leon Wiemers <leonwiemers@gmail.com>
2019-06-12 21:51:57 +0200

    remove unused object

--- src/components/dnd/AWSProduct.tsx
+++ src/components/dnd/AWSProduct.tsx
@@ -7,19 +7,14 @@ export interface AWSProductProps {
   text: string;
   color: string;
 }
-const colors: any = {
-  "s3": "#3F8624",
-  "dynamodb": "#3B48CC"
-}
 
 export default class AWSProduct extends React.Component<AWSProductProps, {}> {
   render() {
     return (
-      <div className={css.awsProduct} style={{ backgroundColor: this.props.color}}>
+      <div className={css.awsProduct} style={{backgroundColor: this.props.color}}>
         <img src={require(`assets/img/${this.props.icon}.svg`) as string} className={css.awsProduct} />
         <span className={css.awsProductText}>{this.props.text}</span>
       </div>
-
     );
   }
 }
