633ea152842c791dbed049d02e97a18d9ca4b978
Dominic Schialer <d.schialer@campus.tu-berlin.de>
2019-06-12 21:14:55 +0200

    updated iam & shield

--- src/levels/LevelElements.tsx
+++ src/levels/LevelElements.tsx
@@ -21,4 +21,6 @@ export type AWSProductLevelElement = "s3" | "dynamodb";
 export const allAWSProducts: Record<AWSProductLevelElement, JSX.Element> = {
   s3: <AWSPaletteItem id="s3" icon="s3" text="S3" color="#3F8624" key="s3_paletteItem" />,
   dynamodb: <AWSPaletteItem id="dynamodb" icon="dynamodb" text="DynamoDB" color="#3B48CC" key="dynamodb_paletteItem" />,
+  iam: <AWSPaletteItem id="iam" icon="iam" text="IAM" color="#D6242D" key="iam_paletteItem" />,
+  shield: <AWSPaletteItem id="shield" icon="shield" text="AWS Shield" color="#D6242D" key="shield_paletteItem" />
 }
