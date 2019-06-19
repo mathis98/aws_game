fc1333ad431627e879a4f69bd0d997879bb966f5
Mathis <mathis@juvigo.de>
2019-06-19 21:27:06 +0200

    Update Redux level states. state is now persistent
    there also now is the option to pick up where you left off

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,22 +4,21 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux';
-import {ScoreState} from '../reducers/score';
+import { ScoreState, scoreType } from '../reducers/score';
 
 const css = require('./Header.css');
-const sum = (arr: any) => {
+const sum = (arr: scoreType[]) => {
   let sum = 0;
   for(let i of arr) sum += i.points;
   return sum;
 }
 
 export interface HeaderProps {
-  score: number[];
+  score: scoreType[];
 }
 
 class Header extends React.Component<HeaderProps, {}> {
   render() {
-    console.log(this.props.score);
     return (
       <AppBar position="static" className={css.fixed_size_app_bar}>
         <Toolbar className={css.toolBar}>
