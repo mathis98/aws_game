81d12f13e1e0520bfd7091c4d96ea71ffc228268
mbauknecht <43986726+mbauknecht@users.noreply.github.com>
2019-06-19 23:12:21 +0200

    Revert "selectionDesign V1"
    This reverts commit e15e5efa09164f7ffed6920cc004cded3d3331ba.

--- src/pages/LevelsPage.tsx
+++ src/pages/LevelsPage.tsx
@@ -3,22 +3,15 @@ import { Link } from 'react-router-dom';
 import PageWrapper from 'components/PageWrapper';
 import LinkButton from 'components/LinkButton';
 import Typography from '@material-ui/core/Typography';
-import ArrowBack from '@material-ui/icons/Lock';
-import Lock from '@material-ui/icons/ArrowBack';
-import CheckCircle from '@material-ui/icons/CheckCircle';
+import ArrowBack from '@material-ui/icons/ArrowBack';
 
 export default class LevelsPage extends React.Component {
   render() {
     return (
       <PageWrapper>
         <Typography variant="h4" gutterBottom>
-          Level 
+          Level wählen
         </Typography>
-        <p>
-          <LinkButton to="https://slsgame.janbe.eu/levels/1" variant="contained" color="primary">
-            <Lock/> Level 1
-          </LinkButton>
-        </p>
         <p>
           <LinkButton to="/" variant="contained" color="primary">
             <ArrowBack/> Zurück
