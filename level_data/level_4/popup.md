a53cf9a55997d1a602d54efac855385f080d1e9c
pseidemann <paul.seidemann@gmail.com>
2019-06-20 16:15:09 +0200

    implement level 4

---
+++ level_data/level_4/popup.md
@@ -0,0 +1,5 @@
+## Level 4: Lambda Functions
+
+Der Fotograf aus Level 2 möchte die Meta Elemente aus den Bildern nun noch zusätzlich in einer Datenbank einpflegen, um später alle Bilder finden zu können, die innerhalb eines Tages, an einem bestimmten Ort oder mit einer bestimmten Kamera bzw. Objektiv geschossen wurden. Jedes Mal wenn ein Bild hochgeladen wird, wird eine Lambda funktion getriggert, die die Meta Daten ausliest und in die Datenbank eingepflegt.
+
+Legen Sie los!
---
+++ src/levels/level4.tsx
@@ -0,0 +1,73 @@
+import { Level, LevelFeedback, LevelState } from './level'
+import * as React from 'react'
+
+const level4: Level ={
+  columns: 4,
+  rows: 1,
+  gap: "2em",
+  elements: [
+    {
+      position: {
+        column: 1,
+        row: 1
+      },
+      id: "camera",
+      icon: "camera"
+    },
+    {
+      position: {
+        column: 2,
+        row: 1
+      },
+      id: "s3",
+      droppable: true
+    },
+    {
+      position: {
+        column: 3,
+        row: 1
+      },
+      id: "lambda",
+      droppable: true
+    },
+    {
+      position: {
+        column: 4,
+        row: 1
+      },
+      id: "dynamodb",
+      droppable: true
+    }
+  ],
+  relations: [
+    {
+      sourceId: "camera",
+      targetId: "s3",
+      sourceAnchor: "right",
+      targetAnchor: "left"
+    },
+    {
+      sourceId: "s3",
+      targetId: "lambda",
+      sourceAnchor: "right",
+      targetAnchor: "left"
+    },
+    {
+      sourceId: "lambda",
+      targetId: "dynamodb",
+      sourceAnchor: "right",
+      targetAnchor: "left"
+    }
+  ],
+  awspalette: ["s3", "dynamodb", "lambda"],
+  validator: Level4Validator
+}
+
+function Level4Validator(state: LevelState): LevelFeedback {
+  if (state.s3 === 's3' && state.lambda === 'lambda' && state.dynamodb === 'dynamodb') {
+    return {correct: true, feedbackComponent: <span>Sehr gut.</span>};
+  }
+  return {correct: false, feedbackComponent: <span>Leider nicht richtig.</span>};
+}
+
+export default level4;
