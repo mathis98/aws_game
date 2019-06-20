df565e3c3c8a8c059444ec7ca421ad9abc2e7d12
Jan Be <jan.becksy@gmail.com>
2019-06-20 15:09:56 +0000

    Implement Level 5

---
+++ src/levels/level5.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { Level, LevelState, LevelFeedback } from './level';
+
+const level5: Level = {
+  columns: 5,
+  rows: 4,
+  gap: "1em",
+  elements: [
+    {
+      position: {
+        column: 1,
+        row: 1,
+      },
+      id: "customer",
+      icon: "customer",
+    },
+    {
+      position: {
+        column: 2,
+        row: 1,
+      },
+      id: "ses",
+      droppable: true,
+    },
+    {
+      position: {
+        column: 2,
+        row: 2,
+      },
+      id: "lambdaTensorflow",
+      droppable: true,
+    },
+    {
+      position: {
+        column: 3,
+        row: 2,
+      },
+      id: "supportEmployee",
+      icon: "supportEmployee",
+    },
+  ],
+  relations: [
+    {
+      sourceId: "customer",
+      targetId: "ses",
+      sourceAnchor: "right",
+      targetAnchor: "left",
+    },
+    {
+      sourceId: "ses",
+      targetId: "lambdaTensorflow",
+      sourceAnchor: "bottom",
+      targetAnchor: "top",
+    },
+    {
+      sourceId: "lambdaTensorflow",
+      targetId: "supportEmployee",
+      sourceAnchor: "right",
+      targetAnchor: "left",
+    },
+  ],
+  awspalette: ["s3", "dynamodb", "iam", "shield", "ses", "lambdaTensorflow"],
+  validator: Level5Validator,
+};
+
+function Level5Validator(state: LevelState): LevelFeedback {
+  if (state.ses === "ses" && state.lambdaTensorflow === "lambdaTensorflow") {
+    return {correct: true, stars: 3};
+  } else {
+    return {correct: false, feedbackComponent: <span>Das funktioniert nicht!</span>};
+  }
+
+  return {correct: false};
+}
+
+export default level5;
--- src/levels/levels.tsx
+++ src/levels/levels.tsx
@@ -2,5 +2,6 @@ import level1 from 'levels/level1';
 import level2 from 'levels/level2';
 import level3 from 'levels/level3';
 import level4 from 'levels/level4';
+import level5 from 'levels/level5';
 
-export default [level1, level2, level3, level4];
+export default [level1, level2, level3, level4, level5];
