65409a9f3eed25c67ff8ab602a9f5dcf0eb075c6
Mathis <mathis.arend@gmail.com>
2019-06-23 20:32:29 +0200

    Fix redux-devtools-extension bug

--- src/app.tsx
+++ src/app.tsx
@@ -23,7 +23,7 @@ const store = createStore(
   persistedState,
   compose(
     applyMiddleware(preventWrongLevel),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    ((window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()) || compose,
   ),
 );
 
