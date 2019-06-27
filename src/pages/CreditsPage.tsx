0026677e2ee4afc8cba7a02169a0c0f2b253485a
Jan Beckschewe <jan.becksy@gmail.com>
2019-06-27 21:59:09 +0200

    Fix randomness of the shuffle

--- src/pages/CreditsPage.tsx
+++ src/pages/CreditsPage.tsx
@@ -3,8 +3,16 @@ import * as React from "react";
 import { Typography } from "@material-ui/core";
 
 export const CreditsPage = (props: any) => {
+  const shuffleArray = (array: any[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+
   const members = ["Paul Seidemann", "Conrad Klaus", "Mark Bauknecht ", "Mathis Arend", "Leon Wiemers", "Jan Beckschewe", "Dominic Schialer"];
-  members.sort((a, b) => 0.5 - Math.random());
+  shuffleArray(members);
+
 
   return <PageWrapper>
     <Typography variant="h4">
