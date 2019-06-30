c631d46f077e1c4fdef3c5bddf3f001a55223cb5
Jan Beckschewe <jan.becksy@gmail.com>
2019-06-30 14:11:24 +0200

    Add credits for the icons
    looks not good yet, but wanted to get it out to

---
+++ assets/img/sources.tsx
@@ -0,0 +1,56 @@
+export interface Source {
+  name: string;
+  creator: string;
+  url: string;
+}
+
+const sources: Source[] = [
+  {
+    "name": "browser",
+    "creator": "Smashicons",
+    "url": "https://www.flaticon.com/free-icon/web_1034609",
+  },
+  {
+    "name": "camera",
+    "creator": "Smashicons",
+    "url": "https://www.flaticon.com/free-icon/photo-camera_149098",
+  },
+  {
+    "name": "customer",
+    "creator": "Freepik",
+    "url": "https://www.flaticon.com/free-icon/business-person-silhouette-wearing-tie_33750",
+  },
+  {
+    "name": "documents",
+    "creator": "Smashicons",
+    "url": "https://www.flaticon.com/free-icon/folder_149333",
+  },
+  {
+    "name": "shop",
+    "creator": "Freepik",
+    "url": "https://www.flaticon.com/free-icon/shop_1067602",
+  },
+  {
+    "name": "supportEmployee",
+    "creator": "Good Ware",
+    "url": "https://www.flaticon.com/free-icon/support_771601",
+  },
+  {
+    "name": "weatherStation",
+    "creator": "Freepik",
+    "url": "https://www.flaticon.com/free-icon/wind-sign_1851004",
+  },
+  // {
+  //   "name": "Smashicons",
+  //   "creator": "Freepik",
+  //   "url": "https://www.flaticon.com/free-icon/globe_1034591",
+  // },
+  // {
+  //   "name": "weatherData",
+  //   "creator": "Pause08",
+  //   "url": "https://www.flaticon.com/free-icon/cloud_860276",
+  // },
+];
+
+export default sources;
+
--- src/pages/CreditsPage.tsx
+++ src/pages/CreditsPage.tsx
@@ -1,8 +1,12 @@
-import PageWrapper from "components/PageWrapper";
 import * as React from "react";
-import { Typography } from "@material-ui/core";
+import { Button, Container, Typography } from "@material-ui/core";
+import { ArrowBack as ArrowBackIcon } from "@material-ui/icons";
+import { withRouter } from "react-router";
+import sources from "../../assets/img/sources";
 
-export const CreditsPage = (props: any) => {
+const css = require("./CreditsPage.css");
+
+export const CreditsPage = withRouter((props: any) => {
   const shuffleArray = (array: any[]) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -11,18 +15,52 @@ export const CreditsPage = (props: any) => {
   };
 
   const members = ["Paul Seidemann", "Conrad Klaus", "Mark Bauknecht ", "Mathis Arend", "Leon Wiemers", "Jan Beckschewe", "Dominic Schialer"];
+
+
+  console.log("so", sources);
   shuffleArray(members);
 
+  return <>
+    <Container maxWidth="md" className={css.creditsTitle}>
+      <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+        Credits
+      </Typography>
+    </Container>
 
-  return <PageWrapper>
-    <Typography variant="h4">
-      <div>Projekt von</div>
-    </Typography>
-    <Typography variant="body1">
-      {
-        members.map(item => <span key={item}>{item}<br/></span>)
-      }
-    </Typography>
+    <div className={css.creditsWrapper}>
+      <Container maxWidth="xs">
+        <Typography variant="h4">
+          Ein Projekt von
+        </Typography>
+        <Typography variant="body1">
+          {
+            members.map(item => <span key={item}>{item}<br/></span>)
+          }
+        </Typography>
 
-  </PageWrapper>;
-};
+
+        <Typography variant="h4">
+          Icons von
+        </Typography>
+        <Typography variant="body1">
+          {
+            sources.map((item: any) =>
+              <span key={item.name}>
+                <a href={item.url}>
+                  <img className={css.iconCreditsImage} width={50} src={require(`../../assets/img/${item.name}.svg`)}/> {item.creator}
+                </a><br/>
+              </span>)
+          }
+        </Typography>
+
+      </Container>
+    </div>
+
+    <Container maxWidth="md">
+      <Button variant="outlined" color="primary" className={css.backButton} onClick={() => props.history.push("/")}>
+        <ArrowBackIcon className={css.backIcon}/>
+        Zurück
+      </Button>
+    </Container>
+  </>;
+});
