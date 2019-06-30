f29de6d2a5f613ebc0f16b9c3417b6919c686df6
mbauknecht <43986726+mbauknecht@users.noreply.github.com>
2019-06-30 23:14:51 +0200

    Icon added

--- src/levels/LevelElements.tsx
+++ src/levels/LevelElements.tsx
@@ -3,7 +3,7 @@ import IconElement from 'components/IconElement';
 import AWSPaletteItem from 'components/dnd/AWSPaletteItem';
 
 export type IconLevelElement = "cam" | "bpol" | "camera" | "documents" | "shop" | "customer" |
-                               "users" | "gameserver" | "supportEmployee" | "weatherStation";
+                               "users" | "gameserver" | "supportEmployee" | "weatherStation" | "cactus" ;
 
 function getIcon(filename: IconLevelElement) {
   return require(`../../assets/img/${filename}.svg`)
@@ -18,13 +18,14 @@ export const allIcons: Record<IconLevelElement, JSX.Element> = {
   shop: <IconElement image={getIcon('shop')} description="Shop" />,
   customer: <IconElement image={getIcon('customer')} description="Kunde" />,
   users: <IconElement image={getIcon('users')} description="Benutzer" />,
+  cactus: <IconElement image={getIcon('cactus')} description="Wunderpflanze" />,
   gameserver: <IconElement image={getIcon('gameserver')} description="Gameserver" />,
   supportEmployee: <IconElement image={getIcon('supportEmployee')} description="Support-Mitarbeiter" color="#CD912D"/>,
   weatherStation: <IconElement image={getIcon('weatherStation')} description="Wetterstation" color="#1498C3"/>,
 };
 
 
-export type AWSProductLevelElement = "s3" | "dynamodb" | "iam" | "shield" | "ses" | "lambda" | "lambdaTensorflow" | "lambda_image_metadata" | "kinesis" | "cognito";
+export type AWSProductLevelElement = "s3" | "dynamodb" | "iam" | "shield" | "ses" | "sns" | "lambda" | "lambdaTensorflow" | "lambda_image_metadata" | "kinesis" | "cognito"| "IoTCore" | "quicksight" | "sagemaker" | "analytics";
 
 export const allAWSProducts: Record<AWSProductLevelElement, JSX.Element> = {
   s3: <AWSPaletteItem id="s3" icon="s3" text="S3" color="#3F8624" key="s3_paletteItem" />,
@@ -33,6 +34,11 @@ export const allAWSProducts: Record<AWSProductLevelElement, JSX.Element> = {
   shield: <AWSPaletteItem id="shield" icon="shield" text="AWS Shield" color="#D6242D" key="shield_paletteItem" />,
   cognito: <AWSPaletteItem id="cognito" icon="cognito" text="cognito" color="#D6242D" key="cognito_paletteItem" />,
   ses: <AWSPaletteItem id="ses" icon="ses" text="SES" color="#445EE0" key="ses_paletteItem" />,
+  sns: <AWSPaletteItem id="sns" icon="ses" text="SNS" color="#445EE0" key="sns_paletteItem" />,
+  IoTCore: <AWSPaletteItem id="IoTCore" icon="IoTCore" text="IoTCore" color="#445EE0" key="IoTCore_paletteItem" />,
+  quicksight: <AWSPaletteItem id="quicksight" icon="quicksight" text="quicksight" color="#445EE0" key="quicksight_paletteItem" />,
+  sagemaker: <AWSPaletteItem id="sagemaker" icon="sagemaker" text="sagemaker" color="#445EE0" key="sagemaker_paletteItem" />,
+  analytics: <AWSPaletteItem id="analytics" icon="analytics" text="analytics" color="#445EE0" key="analytics_paletteItem" />,
   lambda: <AWSPaletteItem id="lambda" icon="lambda" text="Lambda" color="#D86613" key="lambda_paletteItem" />,
   lambdaTensorflow: <AWSPaletteItem id="lambdaTensorflow" icon="lambdaTensorflow" text="Lambda für Tensorflow" color="#DC710E" key="lambdaTensorflow_paletteItem" />,
   lambda_image_metadata: <AWSPaletteItem id="lambda_image_metadata" icon="lambda" text="Lambda" color="#D86613" key="lambda_paletteItem" />,
