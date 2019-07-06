d26f674215654251ba1f2a472bbb9bb35bc910e2
Jan Beckschewe <jan.becksy@gmail.com>
2019-07-06 11:58:29 +0200

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,8 @@ module.exports = {
     },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      favicon: "./src/favicon.ico",
     }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
